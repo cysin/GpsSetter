736fc222f3a8ecf9b2dfcad1c6170efbe9481995
star <anonymous@none.com>
2024-12-30 23:26:33 +0800

    updated hooks, and yukihookapi to latest trunk version

--- app/src/main/java/com/cysindex/telequant/xposed/LocationHook.kt
+++ app/src/main/java/com/cysindex/telequant/xposed/LocationHook.kt
@@ -4,7 +4,10 @@ import android.annotation.SuppressLint
 import android.app.AndroidAppHelper
 import android.app.PendingIntent
 import android.content.Context
+import android.os.Message
 import android.location.Location
+import android.net.wifi.ScanResult
+import android.net.wifi.WifiSsid
 import android.location.LocationListener
 import android.location.LocationManager
 import android.location.LocationRequest
@@ -12,10 +15,28 @@ import com.cysindex.telequant.BuildConfig
 import com.cysindex.telequant.gsApp
 import com.cysindex.telequant.xposed.LocationHook.hook
 import com.highcapable.yukihookapi.hook.entity.YukiBaseHooker
-import com.highcapable.yukihookapi.hook.log.loggerW
 import com.highcapable.yukihookapi.hook.type.java.BooleanType
 import com.highcapable.yukihookapi.hook.type.java.DoubleType
 import com.highcapable.yukihookapi.hook.type.java.FloatType
+import com.highcapable.yukihookapi.YukiHookAPI
+import com.highcapable.yukihookapi.annotation.xposed.InjectYukiHookWithXposed
+import com.highcapable.yukihookapi.hook.core.annotation.LegacyResourcesHook
+import com.highcapable.yukihookapi.hook.factory.applyModuleTheme
+import com.highcapable.yukihookapi.hook.factory.constructor
+import com.highcapable.yukihookapi.hook.factory.field
+import com.highcapable.yukihookapi.hook.factory.method
+import com.highcapable.yukihookapi.hook.factory.registerModuleAppActivities
+import com.highcapable.yukihookapi.hook.log.YLog
+import com.highcapable.yukihookapi.hook.type.android.ActivityClass
+import com.highcapable.yukihookapi.hook.type.android.BundleClass
+import com.highcapable.yukihookapi.hook.type.java.ByteArrayType
+import com.highcapable.yukihookapi.hook.type.java.IntType
+import com.highcapable.yukihookapi.hook.type.java.ListClass
+import com.highcapable.yukihookapi.hook.type.java.StringArrayClass
+import com.highcapable.yukihookapi.hook.type.java.StringClass
+import com.highcapable.yukihookapi.hook.type.java.UnitType
+import com.highcapable.yukihookapi.hook.xposed.bridge.event.YukiXposedEvent
+import com.highcapable.yukihookapi.hook.xposed.proxy.IYukiHookXposedInit
 import de.robv.android.xposed.XposedBridge
 import de.robv.android.xposed.XposedHelpers
 import kotlinx.coroutines.Dispatchers
@@ -38,7 +59,8 @@ object LocationHook : YukiBaseHooker() {
     private val settings = Xshare()
     var mLastUpdated: Long = 0
     private const val className = "android.location.Location"
-    private val ignorePkg = arrayListOf("com.android.location.fused",BuildConfig.APPLICATION_ID)
+    //private val ignorePkg = arrayListOf("com.android.location.fused",BuildConfig.APPLICATION_ID)
+    private val ignorePkg = arrayListOf(BuildConfig.APPLICATION_ID)
 
     private val context by lazy { AndroidAppHelper.currentApplication() as Context }
 
@@ -65,6 +87,7 @@ object LocationHook : YukiBaseHooker() {
     @SuppressLint("NewApi")
     override fun onHook() {
 
+        /*
         loadSystem {
             if (settings.isStarted && (settings.isHookedSystem && !ignorePkg.contains(packageName))) {
                 if (System.currentTimeMillis() - mLastUpdated > 200){
@@ -148,7 +171,8 @@ object LocationHook : YukiBaseHooker() {
                             try {
                                 HiddenApiBypass.invoke(location.javaClass, location, "setIsFromMockProvider", false)
                             } catch (e: Exception) {
-                                loggerW("LocationHook:- ","GS: Not possible to mock  $e")
+                                YLog.warn("LocationHook:- GS: Not possible to mock  $e")
+                                //loggerW("LocationHook:- ","GS: Not possible to mock  $e")
                             }
                             args[0] = location
 
@@ -160,7 +184,400 @@ object LocationHook : YukiBaseHooker() {
 
             }
         }
+        */
 
+        /*
+        "com.qualcomm.location.LocationService".toClass().apply {
+            method {
+                name = "handleMessage"
+                param(
+                    android.os.Message::class.java
+                )
+                returnType = BooleanType
+            }.hook {
+                before {
+                    if (settings.isStarted && !ignorePkg.contains(packageName)) {
+                        lateinit var msg: Message
+                        msg = args(0).any() as Message
+                        msg.what = 0
+                        args[0] = msg
+                        XposedBridge.log("[${packageName}] - 'handleMessage'")
+                    }
+                }
+
+            }
+        }
+        */
+
+        "android.net.wifi.WifiManager".toClass().apply {
+            method {
+                name = "getScanResults"
+                emptyParam()
+                returnType = ListClass
+            }.hook {
+                before {
+                    if(settings.isStarted && !ignorePkg.contains(packageName)) {
+                        val emptyList = emptyList<ScanResult>()
+                        replaceTo(emptyList)
+                        result = emptyList
+                        XposedBridge.log("[${packageName}] - getScanResults")
+
+                    }
+                }
+            }
+        }
+
+        "android.net.wifi.ScanResult".toClass().apply {
+            method {
+                name = "getWifiSsid"
+                emptyParam()
+                returnType = WifiSsid::class.java
+            }.hook {
+                before {
+                    if(settings.isStarted && !ignorePkg.contains(packageName)) {
+                        replaceTo(null)
+                        result = null
+                        XposedBridge.log("[${packageName}] - getWifiSsid")
+
+                    }
+                }
+            }
+        }
+
+        "android.net.wifi.WifiSsid".toClass().apply {
+            method {
+                name = "getBytes"
+                emptyParam()
+                returnType = ByteArrayType
+            }.hook {
+                before {
+                    if(settings.isStarted && !ignorePkg.contains(packageName)) {
+                        val emptyByteArray = byteArrayOf()
+                        replaceTo(emptyByteArray)
+                        result = emptyByteArray
+                        XposedBridge.log("[${packageName}] - getBytes")
+
+                    }
+                }
+            }
+
+            method {
+                name = "toString"
+                emptyParam()
+                returnType = StringClass
+            }.hook {
+                before {
+                    if(settings.isStarted && !ignorePkg.contains(packageName)) {
+                        val emptyString = ""
+                        replaceTo(emptyString)
+                        result = emptyString
+                        XposedBridge.log("[${packageName}] - toString")
+
+                    }
+                }
+            }
+
+        }
+
+        "android.telephony.CellIdentityLte".toClass().apply {
+            method {
+                name = "getCi"
+                emptyParam()
+                returnType = IntType
+            }.hook {
+                before {
+                    if (settings.isStarted && !ignorePkg.contains(packageName)){
+                        val newcid: Long = 10510196757
+                        replaceTo(newcid)
+
+                        result = newcid
+                        XposedBridge.log("[${packageName}] - getCi")
+                    }
+                }
+            }
+
+            method {
+                name = "getMcc"
+                emptyParam()
+                returnType = IntType
+            }.hook {
+                before {
+                    if (settings.isStarted && !ignorePkg.contains(packageName)){
+                        var newmcc: Int = 460
+                        replaceTo(newmcc)
+
+                        result = newmcc
+                        XposedBridge.log("[${packageName}] - getMcc")
+                    }
+                }
+            }
+
+            method {
+                name = "getMccString"
+                emptyParam()
+                returnType = StringClass
+            }.hook {
+                before {
+                    if (settings.isStarted && !ignorePkg.contains(packageName)){
+                        var newmccstring: String = "460"
+                        replaceTo(newmccstring)
+
+                        result = newmccstring
+                        XposedBridge.log("[${packageName}] - getMccString")
+                    }
+                }
+            }
+
+            method {
+                name = "getMnc"
+                emptyParam()
+                returnType = IntType
+            }.hook {
+                before {
+                    if (settings.isStarted && !ignorePkg.contains(packageName)){
+                        var newmnc: Int = 0
+                        replaceTo(newmnc)
+
+                        result = newmnc
+                        XposedBridge.log("[${packageName}] - getMnc")
+                    }
+                }
+            }
+
+            method {
+                name = "getMncString"
+                emptyParam()
+                returnType = StringClass
+            }.hook {
+                before {
+                    if (settings.isStarted && !ignorePkg.contains(packageName)){
+                        var newmncstring: String = "00"
+                        replaceTo(newmncstring)
+
+                        result = newmncstring
+                        XposedBridge.log("[${packageName}] - getMncString")
+                    }
+                }
+            }
+
+            method {
+                name = "getTac"
+                emptyParam()
+                returnType = IntType
+            }.hook {
+                before {
+                    if (settings.isStarted && !ignorePkg.contains(packageName)){
+                        var newtac: Int = 6815749
+                        replaceTo(newtac)
+
+                        result = newtac
+                        XposedBridge.log("[${packageName}] - getTac")
+                    }
+                }
+            }
+
+            method {
+                name = "getPci"
+                emptyParam()
+                returnType = IntType
+            }.hook {
+                before {
+                    if (settings.isStarted && !ignorePkg.contains(packageName)){
+                        var newpci: Int = 999
+                        replaceTo(newpci)
+
+                        result = newpci
+                        XposedBridge.log("[${packageName}] - getPci")
+                    }
+                }
+            }
+
+            method {
+                name = "toString"
+                emptyParam()
+                returnType = StringClass
+            }.hook {
+                before {
+                    if (settings.isStarted && !ignorePkg.contains(packageName)){
+                        var newstring: String = "-"
+                        replaceTo(newstring)
+
+                        result = newstring
+                        XposedBridge.log("[${packageName}] - toString")
+                    }
+                }
+            }
+
+        }
+
+        "android.location.Location".toClass().apply {
+            method {
+                name = "getLatitude"
+                returnType = DoubleType
+            }.hook {
+                before {
+                    if (System.currentTimeMillis() - mLastUpdated > 200){
+                        updateLocation()
+                    }
+                    if (settings.isStarted && !ignorePkg.contains(packageName)){
+                        replaceTo(newlat)
+                        result = newlat
+                        XposedBridge.log("[${packageName}] - 'getLatitude' : ${newlat}")
+                    }
+                }
+            }
+
+            method {
+                name = "getLongitude"
+                returnType = DoubleType
+            }.hook {
+                before {
+                    if (System.currentTimeMillis() - mLastUpdated > 200){
+                        updateLocation()
+                    }
+                    if (settings.isStarted && !ignorePkg.contains(packageName)){
+                        replaceTo(newlng)
+                        result = newlng
+                        XposedBridge.log("[${packageName}] - 'getLongitude' : ${newlng}")
+                    }
+                }
+            }
+
+            method {
+                name = "getAccuracy"
+                returnType = FloatType
+            }.hook {
+                before {
+                    if (System.currentTimeMillis() - mLastUpdated > 200){
+                        updateLocation()
+                    }
+                    if (settings.isStarted && !ignorePkg.contains(packageName)){
+                        replaceTo(accuracy)
+                        result = accuracy
+                        XposedBridge.log("[${packageName}] - 'getAccuracy' : ${accuracy}")
+                    }
+                }
+            }
+
+            method {
+                name = "isMock"
+                returnType = BooleanType
+            }.hook {
+                before {
+                    if (settings.isStarted && !ignorePkg.contains(packageName)){
+                        replaceToFalse()
+                        result = false
+                        XposedBridge.log("[${packageName}] - 'isMock' ")
+                    }
+                }
+            }
+
+            method {
+                name = "isFromMockProvider"
+                returnType = BooleanType
+            }.hook {
+                before {
+                    if (settings.isStarted && !ignorePkg.contains(packageName)){
+                        replaceToFalse()
+                        result = false
+                        XposedBridge.log("[${packageName}] - 'isFromMockProvider' ")
+                    }
+                }
+            }
+
+            method {
+                name = "set"
+                param(Location::class.java)
+            }.hook {
+                before {
+                    if (System.currentTimeMillis() - mLastUpdated > 200){
+                        updateLocation()
+                    }
+                    if (settings.isStarted && !ignorePkg.contains(packageName)){
+                        lateinit var location: Location
+                        lateinit var originLocation: Location
+                        if (args[0] == null){
+                            location = Location(LocationManager.GPS_PROVIDER)
+                            location.time = System.currentTimeMillis() - 300
+                        }else {
+                            originLocation = args(0).any() as Location
+                            location = Location(originLocation.provider)
+                            location.time = originLocation.time
+                            location.accuracy = accuracy
+                            location.bearing = originLocation.bearing
+                            location.bearingAccuracyDegrees = originLocation.bearingAccuracyDegrees
+                            location.elapsedRealtimeNanos = originLocation.elapsedRealtimeNanos
+                            location.verticalAccuracyMeters = originLocation.verticalAccuracyMeters
+                        }
+
+                        location.latitude = newlat
+                        location.longitude = newlng
+                        location.altitude = 0.0
+                        location.speed = 0F
+                        location.speedAccuracyMetersPerSecond = 0F
+                        XposedBridge.log("[${packageName}] - 'set' lat: ${location.latitude}, lon: ${location.longitude}")
+
+                        /*
+                        try {
+                            HiddenApiBypass.invoke(
+                                location.javaClass,
+                                location,
+                                "isFromMockProvider",
+                                false
+                            )
+                            HiddenApiBypass.invoke(location.javaClass, location, "isMock", false)
+                        } catch (e: Exception) {
+                            YLog.warn(msg = "GS: Not possible to mock  $e", tag = "LocationHook set:- ")
+                        }
+                        */
+
+                        //args().first().set(location)
+                        args[0] = location
+
+                    }
+                }
+
+            }
+
+
+        }
+
+        "android.location.LocationManager".toClass().apply {
+            method {
+                name = "getLastKnownLocation"
+                param(String::class.java)
+            }.hook {
+                before {
+                    if (System.currentTimeMillis() - mLastUpdated > 200){
+                        updateLocation()
+                    }
+                    if (settings.isStarted && !ignorePkg.contains(packageName)) {
+                        val provider = args[0] as String
+                        val location = Location(provider)
+                        location.time = System.currentTimeMillis() - 300
+                        location.latitude = newlat
+                        location.longitude = newlng
+                        location.altitude = 0.0
+                        location.speed = 0F
+                        location.speedAccuracyMetersPerSecond = 0F
+                        XposedBridge.log("[${packageName}] - 'getLastKnowLocation' : lat: ${location.latitude}, lon: ${location.longitude}")
+
+                        /*
+                        try {
+                            HiddenApiBypass.invoke(location.javaClass, location, "isMock", false)
+                            HiddenApiBypass.invoke(location.javaClass, location, "isFromMockProvider", false)
+                        } catch (e: Exception) {
+                            XposedBridge.log("'getLastKnowLocation' GS: Not possible to mock (Pre Q)! $e")
+                        }
+                        */
+
+                        replaceTo(location)
+                        result = location
+                    }
+                }
+            }
+        }
+        /*
         findClass(className).hook {
             injectMember {
                 method {
@@ -287,6 +704,7 @@ object LocationHook : YukiBaseHooker() {
                 }
             }
         }
+        */
 
     }
 
